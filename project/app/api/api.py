d69d58635b29b73f6d75cf7fbe7fc356f0f7360c
Miguel Acevedo <44342378+miguelalb@users.noreply.github.com>
2021-06-26 21:52:50 -0400

    Removed Ping

--- project/app/api/api.py
+++ project/app/api/api.py
@@ -1,13 +1,12 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import category, login, ping, user
+from app.api.endpoints import category, login, user
 from app.core.config import get_settings
 
 settings = get_settings()
 
 router = APIRouter(prefix=settings.API_V1_STR)
 
-router.include_router(ping.router, tags=["Ping"])
 router.include_router(login.router, tags=["Auth"])
 router.include_router(user.router, prefix="/user", tags=["User"])
 router.include_router(category.router, prefix="/category", tags=["Category"])
--- project/tests/test_ping.py
+++
@@ -1,4 +0,0 @@
-def test_ping(test_app):
-    response = test_app.get("/api/v1/ping")
-    assert response.status_code == 200
-    assert response.json() == {"environment": "test", "testing": True, "ping": "pong!"}
