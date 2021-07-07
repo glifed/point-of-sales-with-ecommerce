b71496ee912546d44e3a5b95e6187a6b2ee54304
Miguel Acevedo <44342378+miguelalb@users.noreply.github.com>
2021-07-06 22:08:28 -0400

    Unable to separate CRUD. Back to initial model

--- project/backoffice-api/tests/test_crud.py
+++
@@ -1,9 +0,0 @@
-from app.crud.base import CRUDBase
-
-def test_crudbase_has_get_getall_create_update_delete():
-    crud = CRUDBase()
-    assert hasattr(crud, 'get') and callable(getattr(crud, 'get'))
-    assert hasattr(crud, 'get_all') and callable(getattr(crud, 'get_all'))
-    assert hasattr(crud, 'create') and callable(getattr(crud, 'create'))
-    assert hasattr(crud, 'update') and callable(getattr(crud, 'get_all'))
-    assert hasattr(crud, 'delete') and callable(getattr(crud, 'delete'))
